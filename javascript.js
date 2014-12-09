cde541bf739d2204d405b8b7f9a2ca12c882591f
Sanette Tanaka <sanette.tanaka@gmail.com>
2014-12-09 20:16:08 +0000

    Set up basic shell of coffee website

---
+++ javascript.js
@@ -0,0 +1,164 @@
+function main() {
+	buildProducts(coffeeProductsArray);
+	showProducts();
+}
+
+
+function showProducts() {
+	var products = document.getElementsByClassName("gal4");
+	for(var i = 0; i < products.length; i++) {
+    	products[i].style.display = "inline";
+	}	
+}
+
+function buildProducts(coffeeProductsArray){
+	//get section coffee div
+	var section = document.getElementById("coffee_products");
+	var allCoffeeDivs = "";
+	//set up divs for individual coffee products
+	for (var i = 0; i < coffeeProductsArray.length; i++) {
+		newDiv = "<div id='"+ coffeeProductsArray[i].Id + "' class='gal4'></div>";
+		allCoffeeDivs = allCoffeeDivs.concat(newDiv);
+	}
+	section.innerHTML = allCoffeeDivs;	
+	//iterate over coffee array, and fill in each div tag with coffee information
+	for (var i = 0; i < coffeeProductsArray.length; i++) {
+		myCurrentTag = document.getElementById(coffeeProductsArray[i].Id);
+		var productInfo;
+		productInfo = "<img src='CoffeePhotos/CoffeeProducts/" + coffeeProductsArray[i].Id + ".jpg' alt='Coffee Beans Product'><p>";
+		productInfo = productInfo.concat("<div id = 'CoffeeName'>" + coffeeProductsArray[i].Name + "</div>");
+		productInfo = productInfo.concat("<div id = 'CoffeeVariety'>Variety: " + coffeeProductsArray[i].Variety + "</div>");	
+		productInfo = productInfo.concat("<div id = 'CoffeeRegion'>Region: " + coffeeProductsArray[i].Region + "</div>");	
+		productInfo = productInfo.concat("<div id = 'CoffeeType'>" + coffeeProductsArray[i].Type + "</div>");	
+		productInfo = productInfo.concat("<div id = 'CoffeePrice'>Price: €" + coffeeProductsArray[i].Price + "</div>");	
+		productInfo = productInfo.concat("<button type='button'>Add to Cart</button>");
+		myCurrentTag.innerHTML = productInfo;
+		//add region class
+		if (coffeeProductsArray[i].Region == "Asia") {
+			myCurrentTag.className = myCurrentTag.className + " asia";
+		} else if (coffeeProductsArray[i].Region == "Africa") {
+			myCurrentTag.className = myCurrentTag.className + " africa";
+		} else if (coffeeProductsArray[i].Region == "Oceania") {
+			myCurrentTag.className = myCurrentTag.className + " oceania";
+		} else if (coffeeProductsArray[i].Region == "Latin America") {
+			myCurrentTag.className = myCurrentTag.className + " latinamerica";
+		} else {
+			break;
+		}
+		//add decaf or regular class
+		if (coffeeProductsArray[i].Type == "Regular") {
+			myCurrentTag.className = myCurrentTag.className + " regular";
+		} else if (coffeeProductsArray[i].Type == "Decaf") {
+			myCurrentTag.className = myCurrentTag.className + " decaf";
+		} else {
+			break;
+		}
+	}
+}
+
+function filter() {
+	var updatedCoffeeProductsArray = [];
+	//check first if price radio button is checked, because then list must be reordered
+    var priceChecked = checkPriceStatus();
+	if (priceChecked) {
+		updatedCoffeeProductsArray = sortByPrice(coffeeProductsArray);
+		buildProducts(updatedCoffeeProductsArray);
+		showProducts();
+	}
+	//Get product elements with the gal4 class
+	var elems = document.getElementsByClassName("gal4");
+	//iterate over each coffee product
+	for(var i = 0; i < elems.length; i++) {
+	 	//Set every product equal to display = none
+    	elems[i].style.display = "none";
+		var regionStatus = checkRegion(elems[i]);
+		var coffeeTypeStatus = filterByCoffeeType(elems[i]);
+		displayFilteredProducts(regionStatus, coffeeTypeStatus, elems[i]);
+	}
+
+
+}
+ 
+function checkRegion(elems) {
+	var region = document.getElementById("regionDropdown");
+	var regionValue = region.options[region.selectedIndex].value;
+	//If element contains selected region, return true for that region
+	if (regionValue == "") {
+		return true;
+	} else if(elems.classList.contains(regionValue)) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function filterByCoffeeType(elems){
+	var decaf = document.getElementById("decafCheckbox");
+	var regular = document.getElementById("regularCheckbox");
+	// Return true if element matches checkbox status
+	if (regular.checked && decaf.checked) {
+		return true;
+	} else if (regular.checked && elems.classList.contains(regular.value)) {
+ 		return true;
+ 	} else if (decaf.checked && elems.classList.contains(decaf.value)){
+		return true;
+ 	} else if (!regular.checked && !decaf.checked){
+ 		return true;
+ 	} else {
+ 		return false;
+ 	}
+}
+
+function checkPriceStatus() {
+	var priceAscend = document.getElementById("priceAscend");
+	var priceDescend = document.getElementById("priceDescend");
+	//check if price checkbox is checked
+	if (priceAscend.checked) {
+		return true;
+	} else if (priceDescend.checked){
+ 		return true;
+ 	} else {
+ 		return false;
+ 	}
+}
+
+function displayFilteredProducts(regionStatus, coffeeTypeStatus, elems) {
+	//if regionStatus and coffeeTypeStatus are both true, show element
+	if (regionStatus && coffeeTypeStatus){
+		elems.style.display = "inline";
+	} else {
+		elems.style.display = "none";
+	}
+}
+
+function sortByPrice(coffeeProductsArray) {
+	var priceAscend = document.getElementById("priceAscend");
+	var priceDescend = document.getElementById("priceDescend");
+	if (priceAscend.checked) {
+		coffeeProductsArray.sort(function(a, b){
+ 			return a.Price-b.Price;
+		})
+ 	} else if (priceDescend.checked) {
+		coffeeProductsArray.sort(function(a, b){
+			return b.Price-a.Price;
+		})
+ 	}
+ 	return coffeeProductsArray;
+}
+
+
+function clearFilters() {
+	//Get product elements with the gal4 class
+	var elems = document.getElementsByClassName("gal4");
+	//iterate over all gal4 elements and switch all displays to inline
+	for(var i = 0; i < elems.length; i++) {
+		elems[i].style.display = "inline";
+	}
+	//Uncheck decaf and regular boxes.
+	document.getElementById("decafCheckbox").checked = false;
+	document.getElementById("regularCheckbox").checked = false;
+	document.getElementById("regionDropdown").selectedIndex = "0";
+	document.getElementById("regionDropdown").selectedIndex = "0";
+	document.getElementById("priceAscend").checked = false;
+	document.getElementById("priceDescend").checked = false;
+}
